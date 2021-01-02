77b94b707f27079721863465b68a6148139eb3fc
Dhruv-Vinod <70434496+Dhruv-Vinod@users.noreply.github.com>
2021-01-02 12:01:46 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+var effects = ["Respiratory diseases","Cardiovascular damage","Fatigue headaches and anxiety","Irritation of the eyes nose and throat","Damage to reproductive organs","Harm to the liver spleen and bloods","Nervous system damage"]
+
+for(var i=0;i<length.effects;i++)
+
+
+
